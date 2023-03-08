0407bb2a725c0bc40c504ece9370076017bc8cde
bryantan <bet20@ic.ac.uk>
2023-03-08 12:03:40 +0000

    updated notebook

--- src/cordic_unroll/run_tb.sh
+++ src/cordic_unroll/run_tb.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-# iverilog -g 2005 -o ../../out/a cordic_unroll_slave_variable.v
-iverilog -g 2005 -s tb -o ../../out/tb cordic_unroll_slave_variable_tb.v cordic_unroll_slave_variable.v
+iverilog -g 2005 -o ../../out/a cordic_unroll1_slave_variable.v
+# iverilog -g 2005 -s tb -o ../../out/tb cordic_unroll_slave_variable_tb.v cordic_unroll_slave_variable.v
 # iverilog -g 2005 -s tb -o ../../out/tb cordic_unroll_slave_tb.v cordic_unroll_slave.v
 ../../out/tb
 
