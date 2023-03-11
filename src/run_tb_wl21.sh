427eb0fe5bbe1807832fdb05493e7bb0d0c15a1e
bryantan <bet20@ic.ac.uk>
2023-03-11 12:42:38 +0000

    added expected inputs and outputs for inner function testbench

--- src/run_tb_wl21.sh
+++ src/run_tb_wl21.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 # iverilog -g 2005 -s tb -o ../../../out/syntax syntax.v
 # ../../../out/syntax
 
-# VARIANT="cordic_unroll1_var"
-# iverilog -g 2005 -s tb -o bin/tb_unroll_var tb/tb_unroll_var.v wl_21/cordic_unroll/${VARIANT}.v wl_21/helpers/*
-# bin/tb_unroll_var 1> out/${VARIANT}.txt
+VARIANT="cordic_unroll1_var"
+iverilog -g 2005 -s tb -o bin/tb_unroll_var tb/tb_unroll_var.v wl_21/cordic_unroll/${VARIANT}.v wl_21/helpers/*
+bin/tb_unroll_var 1> out/${VARIANT}.txt
 
 # VARIANT="cordic_unroll2_var"
 # iverilog -g 2005 -s tb -o bin/tb_unroll_var tb/tb_unroll_var.v wl_21/cordic_unroll/${VARIANT}.v wl_21/helpers/*
@@ -21,8 +21,8 @@ set -euo pipefail
 # iverilog -g 2005 -s tb -o bin/tb_unroll_var tb/tb_unroll_var.v wl_21/cordic_unroll/${VARIANT}.v wl_21/helpers/*
 # bin/tb_unroll_var 1> out/${VARIANT}.txt
 
-iverilog -g 2005 -s tb -o bin/tb_pipeline tb/tb_pipeline.v wl_21/cordic_pipeline/cordic_pipeline.v wl_21/helpers/*
-bin/tb_pipeline 1> out/cordic_pipeline.txt
+# iverilog -g 2005 -s tb -o bin/tb_pipeline tb/tb_pipeline.v wl_21/cordic_pipeline/cordic_pipeline.v wl_21/helpers/*
+# bin/tb_pipeline 1> out/cordic_pipeline.txt
 
 
 
